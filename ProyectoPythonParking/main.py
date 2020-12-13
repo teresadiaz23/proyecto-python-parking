2787dc32b4048ec7b68ff7fe4ecbf517680e5b5d
Teresa Diaz <tdayuga10@gmail.com>
2020-12-13 17:50:16 +0100

    Pequeños cambios

--- ProyectoPythonParking/main.py
+++ ProyectoPythonParking/main.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from models.MesIncorrecto import MesIncorrecto
 
 print(main_controller.bienvenida())
-#print(main_controller.menu_principal())
+
 continuar = True
 
 while continuar:
@@ -152,37 +152,6 @@ while continuar:
 
 print("\nGracias por usar el parking\nBuen viaje")
 
-# if(op == 1):
-#     pass
-#
-# elif(op == 2):
-#     pass
-#
-# elif(op == 3):
-#     pass
-#
-# elif(op == 0):
-#     continuar = False
-#
-# cliente = ClienteAbonado("1234", "Teresa", "Diaz", "123456", "teresa@email.com", None)
-#
-# print(cliente.nombre)
-#
-# fecha1 = datetime(2020, 12, 10, 18, 0, 0)
-# fecha = date(2020,12,12)
-# print(fecha + timedelta(days=30))
-# print(fecha.month)
-# print(fecha + relativedelta(months=1))
-# print(datetime.now() + relativedelta(months=1))
-# fecha2 = datetime.now()
-# tiempo = fecha2 -fecha1
-#
-# print(floor(tiempo.total_seconds()/60))
-#
-# #print(hoy.day, hoy.month, hoy.year)
-#
-# alea = randint(111111, 999999)
-# print(alea)
 
 
 
